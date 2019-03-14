7fec2272608b897ed5a91dcf2b1a6a1208ac39fe
Andy H <ahumburg@gmail.com>
2019-03-13 20:03:19 -0500

    things happening

--- Andy/config.py
+++ Andy/config.py
@@ -1,5 +1,8 @@
 # Enter your API key
 # API: NCAAMB Trial
-# API Key: API KEY: xb62p9vmshdq46tqyskbkcgu
+# old API Key: API KEY: xb62p9vmshdq46tqyskbkcgu
+# new API KEY: mg9jrv2qw98ne5hkjybxa889
+# new again - not ready yet - API KEY: 9z3h7hcahqactppwxz8v36mb
+# new api - a@y Key: nn7mcjdxcjj92d9xw72xxm23
 
-api_key="xb62p9vmshdq46tqyskbkcgu"
+api_key = "nn7mcjdxcjj92d9xw72xxm23"
